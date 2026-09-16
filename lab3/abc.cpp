fdd7b661584053953ead849d01ea85c6f2d2c851
ThisComments <dimajizov198428@mail.ru>
2026-09-16 11:06:51 +0300

    feat: add 3-numbers summation program

---
+++ lab3/abc.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    int a = 0, b = 0, c = 0;
+    std::cout << "Enter 3 numbers" << std::endl;
+    std::cin >> a >> b >> c;
+    std::cout << a + b + c << std::endl;
+    return 0;
+}
