7926335c990a21f27db9343cf2c4ab77d45b93c6
ThisComments <dimajizov198428@mail.ru>
2026-09-16 11:21:47 +0300

    feat: add linear and quadratic equation solvers

---
+++ lab3/square_equation.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    float a = 0.0f;
+    float b = 0.0f;
+    float c = 0.0f;
+
+    std::cout << "Enter coefficients a, b and c for 'ax^2 + bx + c = 0': ";
+    std::cin >> a >> b >> c;
+
+    float D = b * b - 4 * a * c;
+    if (D > 0) 
+    {
+        float x_1 = (-b + std::sqrt(D)) / 2 * a;
+        float x_2 = (-b - std::sqrt(D)) / 2 * a;
+        std::cout << "Solution:" << std::endl << "x_1 = " << x_1 << std::endl << "x_2 = " << x_2 << std::endl;
+    } 
+    else if (D == 0)
+    {
+        float x = -b / 2 * a;
+        std::cout << "Solution x = " << x << std::endl;
+    }
+    else
+    {
+        std::cout << "There are no real solution." << std::endl;
+    }
+
+    return 0;
+}
