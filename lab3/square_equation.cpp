12248990f1e08658e33b14e61fdb6e0a78771bf6
ThisComments <dimajizov198428@mail.ru>
2026-09-23 10:23:55 +0300

    feat: fixed

--- lab3/square_equation.cpp
+++ lab3/square_equation.cpp
@@ -10,21 +10,29 @@ int main()
     std::cout << "Enter coefficients a, b and c for 'ax^2 + bx + c = 0': ";
     std::cin >> a >> b >> c;
 
-    float D = b * b - 4 * a * c;
-    if (D > 0) 
+    if (a != 0.0f)
     {
-        float x_1 = (-b + std::sqrt(D)) / 2 * a;
-        float x_2 = (-b - std::sqrt(D)) / 2 * a;
-        std::cout << "Solution:" << std::endl << "x_1 = " << x_1 << std::endl << "x_2 = " << x_2 << std::endl;
-    } 
-    else if (D == 0)
-    {
-        float x = -b / 2 * a;
-        std::cout << "Solution x = " << x << std::endl;
+        float D = b * b - 4 * a * c;
+        if (D > 0.0f)
+        {
+            float x_1 = (-b + std::sqrt(D)) / 2 * a;
+            float x_2 = (-b - std::sqrt(D)) / 2 * a;
+            std::cout << "Solution:" << std::endl << "x_1 = " << x_1 << std::endl << "x_2 = " << x_2 << std::endl;
+        } 
+        else if (D == 0.0f)
+        {
+            float x = -b / 2 * a;
+            std::cout << "Solution x = " << x << std::endl;
+        }
+        else
+        {
+            std::cout << "There are no real solution." << std::endl;
+        }
     }
-    else
+    else 
     {
-        std::cout << "There are no real solution." << std::endl;
+        std::cout << "Error: coefficient 'a' cannot be zero." << std::endl;
+        return 1;
     }
 
     return 0;
